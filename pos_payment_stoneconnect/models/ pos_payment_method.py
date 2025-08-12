36025c13e5fe0e9667443ad1582c6b3bd4587f4a
Cleber Santz <clebersantz@gmail.com>
2025-08-12 11:34:25 -0300

    remove old modules

--- pos_payment_stoneconnect/models/ pos_payment_method.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class PosPaymentMethod(models.Model):
-    _inherit = 'pos.payment.method'
-
-    stone_terminal_id = fields.Char("Stone Terminal ID")
-    stone_api_token = fields.Char("Stone API Token")
--- pos_payment_stoneconnect/models/payment_provider.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class PaymentProvider(models.Model):
-    _inherit = 'payment.provider'
-
-    pagarme_api_key = fields.Char("Pagarme API Key")
