4cc4eb2d522e0d5ab4defd1f9d0d9cc87db7bbc5
clebersantz <clebersantz@gmail.com>
2025-09-01 11:03:47 -0300

    Refactor payment provider setup for Pagarme

--- l10n_br_payment_pagarme/__init__.py
+++ l10n_br_payment_pagarme/__init__.py
@@ -3,15 +3,11 @@
 from . import controllers
 from . import models
 
-from odoo import SUPERUSER_ID, api
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
 
 def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    setup_provider(env, 'pagarme')
-
+    setup_provider(cr, registry, 'pagarme')
 
 def uninstall_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    reset_payment_provider(env, 'pagarme')
+    reset_payment_provider(cr, registry, 'pagarme')
